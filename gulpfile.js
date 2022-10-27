5934c5e50ef2de40a22f1e2b9a9e6f9b3e3aa5b7
Nate <natehuang0913@gmail.com>
2022-10-27 11:06:25 +0800

    gulpfile add

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const {
+    src,
+    dest,
+    series,
+    parallel,
+    watch
+} = require('gulp');
+
+
+function defaultTask(cb){
+    console.log('gulp 成功');
+    cb();
+}
+
+exports.c = defaultTask;
+
+//taskA
+function A(cb){
+    console.log('a mission');
+    cb();
+}
+
+//taskB
+function B(cb){
+    console.log('b mission');
+    cb();
+}
+
+exports.sync = series(A, B);
+exports.async = parallel(A, B);
